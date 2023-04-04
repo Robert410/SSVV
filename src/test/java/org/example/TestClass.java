40183e09cdcb2377c9a9f5c1519787c8ed5627d2
911-Brinza-Alina <72062984+911-Brinza-Alina@users.noreply.github.com>
2023-04-04 16:58:11 +0300

    hai ca da

--- src/test/java/org/example/TestClass.java
+++ src/test/java/org/example/TestClass.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.domain.Student;
+import org.example.domain.Tema;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -35,6 +36,38 @@ public class TestClass {
         NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
         NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
         TestClass.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @Test
+    public void addTema_ValidData_CreateSuccessfully() {
+        String nrTema = "100";
+        String descriere = "test";
+        int deadline = 12;
+        int primire = 1;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+        try {
+            service.addTema(tema);
+            assert(true);
+        } catch (ValidationException exception) {
+            System.out.println("Validation exception: " + exception.getMessage());
+            assert(false);
+        }
+    }
+
+    @Test
+    public void addTema_Invalid_nrTema_ThrowsError() {
+        String nrTema = null;
+        String descriere = "test";
+        int deadline = 12;
+        int primire = 2;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+        try {
+            service.addTema(tema);
+            assert(false);
+        } catch (ValidationException exception) {
+            System.out.println("Validation exception: " + exception.getMessage());
+            assert(true);
+        }
     }
 
     @Test
