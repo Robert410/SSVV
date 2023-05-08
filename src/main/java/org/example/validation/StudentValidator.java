f133cfdf7355a085feefa8271dce845c8d234dd8
911-Brinza-Alina <72062984+911-Brinza-Alina@users.noreply.github.com>
2023-05-08 18:36:30 +0300

    Added mockito + integration tests

--- src/main/java/org/example/validation/StudentValidator.java
+++ src/main/java/org/example/validation/StudentValidator.java
@@ -2,6 +2,8 @@ package org.example.validation;
 
 import org.example.domain.Student;
 
+import java.util.Objects;
+
 public class StudentValidator implements Validator<Student> {
 
     /**
@@ -17,7 +19,10 @@ public class StudentValidator implements Validator<Student> {
         if(entity.getID().equals("")) {
             throw new ValidationException("Id incorect!");
         }
-        if(entity.getNume() == ""){
+        if(entity.getNume() == null){
+            throw new ValidationException("Nume incorect!");
+        }
+        if(Objects.equals(entity.getNume(), "")){
             throw new ValidationException("Nume incorect!");
         }
         if(entity.getGrupa() < 0) {
@@ -25,9 +30,6 @@ public class StudentValidator implements Validator<Student> {
         }
         if(entity.getEmail() == null){
             throw new ValidationException("Email incorect!");
-        }
-        if(entity.getNume() == null){
-            throw new ValidationException("Nume incorect!");
         }
         if(entity.getEmail().equals("")){
             throw new ValidationException("Email incorect!");
