90766a973029375cda68533f175480b261bb766d
911-Brinza-Alina <72062984+911-Brinza-Alina@users.noreply.github.com>
2023-05-09 13:48:20 +0300

    Added the tests in the same class

--- src/test/java/org/example/TestClass.java
+++ src/test/java/org/example/TestClass.java
@@ -3,11 +3,7 @@ package org.example;
 import org.example.domain.Nota;
 import org.example.domain.Student;
 import org.example.domain.Tema;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.*;
 import org.example.repository.NotaXMLRepo;
 import org.example.repository.StudentXMLRepo;
 import org.example.repository.TemaXMLRepo;
@@ -16,15 +12,57 @@ import org.example.validation.NotaValidator;
 import org.example.validation.StudentValidator;
 import org.example.validation.TemaValidator;
 import org.example.validation.ValidationException;
-
+import org.mockito.Mock;
 import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
-//@RunWith(JUnit4.class)
 public class TestClass {
 
+    @Mock
+    private StudentValidator studentValidatorMock;
+
+    @Mock
+    private TemaValidator temaValidatorMock;
+
+    @Mock
+    private NotaValidator notaValidatorMock;
+
+    @Mock
+    private StudentXMLRepo studentXMLRepositoryMock;
+
+    @Mock
+    private TemaXMLRepo temaXMLRepositoryMock;
+
+    @Mock
+    private NotaXMLRepo notaXMLRepositoryMock;
+
+    private Service serviceMock;
+
     public static Service service;
+
+    @BeforeEach
+    public void setupMock() {
+        studentValidatorMock = mock(StudentValidator.class);
+        temaValidatorMock = mock(TemaValidator.class);
+        notaValidatorMock = mock(NotaValidator.class);
+        studentXMLRepositoryMock = mock(StudentXMLRepo.class);
+        temaXMLRepositoryMock = mock(TemaXMLRepo.class);
+        notaXMLRepositoryMock = mock(NotaXMLRepo.class);
+        serviceMock = new Service(studentXMLRepositoryMock, studentValidatorMock, temaXMLRepositoryMock, temaValidatorMock, notaXMLRepositoryMock, notaValidatorMock);
+    }
+
+    @AfterEach
+    public void tearDownMock() {
+        studentValidatorMock = null;
+        temaValidatorMock = null;
+        notaValidatorMock = null;
+        studentXMLRepositoryMock = null;
+        temaXMLRepositoryMock = null;
+        notaXMLRepositoryMock = null;
+        serviceMock = null;
+    }
 
     @BeforeAll
     public static void setup() {
@@ -39,6 +77,99 @@ public class TestClass {
         NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
         NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
         TestClass.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    // Incremental Integration testing + Mockito
+    @Test
+    public void addStudent_InvalidNume_ThrowsException() {
+        String studentId = "1";
+        String nume = "";
+        int grupa = 931;
+        String email = "miguel@yahoo.com";
+
+        Student student = new Student(studentId, nume, grupa, email);
+
+        try {
+            doThrow(new ValidationException("Nume incorect!")).when(studentValidatorMock).validate(student);
+            Assertions.assertThrows(ValidationException.class, () -> {
+                serviceMock.addStudent(student);
+            });
+        } catch (ValidationException ve) {
+            ve.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addStudent_Valid_addTema_InvalidDeadline_ThrowsException() {
+        String studentId = "1";
+        String nume = "Miguel";
+        int grupa = 931;
+        String email = "miguel@yahoo.com";
+        Student student = new Student(studentId, nume, grupa, email);
+
+        String nrTema = "1";
+        String descriere = "descriere";
+        int deadline = 20;
+        int primire = 2;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+
+        try {
+            doNothing().when(studentValidatorMock).validate(student);
+            when(studentXMLRepositoryMock.save(student)).thenReturn(null);
+            doThrow(new ValidationException("Deadlineul trebuie sa fie intre 1-14.")).when(temaValidatorMock).validate(tema);
+
+            Student returnedStudent = serviceMock.addStudent(student);
+            Assertions.assertNull(returnedStudent);
+            Assertions.assertThrows(ValidationException.class, () -> {
+                serviceMock.addTema(tema);
+            });
+        } catch (ValidationException ve) {
+            ve.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addStudent_Valid_addTema_Valid_addNota_Valid_ThrowsException() {
+        String studentId = "1";
+        String nume = "Miguel";
+        int grupa = 931;
+        String email = "miguel@yahoo.com";
+        Student student = new Student(studentId, nume, grupa, email);
+
+        String nrTema = "1";
+        String descriere = "descriere";
+        int deadline = 12;
+        int primire = 2;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+
+        String notaId = "nt1";
+        double notaVal = 9.5;
+        LocalDate date = LocalDate.of(2023, 4, 25);
+        Nota nota = new Nota(notaId, studentId, nrTema, notaVal, date);
+
+        try {
+            doNothing().when(studentValidatorMock).validate(student);
+            when(studentXMLRepositoryMock.save(student)).thenReturn(null);
+
+            doNothing().when(temaValidatorMock).validate(tema);
+            when(temaXMLRepositoryMock.save(tema)).thenReturn(null);
+
+            when(studentXMLRepositoryMock.findOne(studentId)).thenReturn(student);
+            when(temaXMLRepositoryMock.findOne(nrTema)).thenReturn(tema);
+
+            Student returnedStudent = serviceMock.addStudent(student);
+            Assertions.assertNull(returnedStudent);
+
+            Tema returnedTema = serviceMock.addTema(tema);
+            Assertions.assertNull(returnedTema);
+
+            Nota returnedNota = serviceMock.addNota(nota, "feedback");
+            Assertions.assertNull(returnedNota);
+            Assertions.assertEquals(9.5, nota.getNota());
+        } catch (ValidationException ve) {
+            ve.printStackTrace();
+        }
     }
 
     // Big-bang Integration testing
